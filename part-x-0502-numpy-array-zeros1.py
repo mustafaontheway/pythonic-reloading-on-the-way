676b1026a5c0515db090b97a1289b1ea18419400
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-12-15 23:15:33 +0300

    Create part-x-0502-numpy-array-zeros1.py

---
+++ part-x-0502-numpy-array-zeros1.py
@@ -0,0 +1,18 @@
+import numpy as np 
+
+my_arr = np.zeros(7, dtype="float16")
+
+my_arr2 = np.zeros((6, 3), dtype="int")
+
+print(my_arr)
+
+print(my_arr2)
+
+# [0. 0. 0. 0. 0. 0. 0.]
+
+# [[0 0 0]
+#  [0 0 0]
+#  [0 0 0]
+#  [0 0 0]
+#  [0 0 0]
+#  [0 0 0]]
