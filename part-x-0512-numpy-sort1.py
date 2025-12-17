8120ca19003ab0eac50e5e0ba0d6ddfaf25cbcb4
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-12-17 19:59:26 +0300

    Create part-x-0512-numpy-sort1.py

---
+++ part-x-0512-numpy-sort1.py
@@ -0,0 +1,13 @@
+import numpy as np 
+
+arr = np.random.randint(-50, 40, 10)
+
+print(arr)
+
+sorted_arr = np.sort(arr)
+
+print(sorted_arr)
+
+# [-50  24  25 -32  12 -32 -37 -20 -48  18]
+
+# [-50 -48 -37 -32 -32 -20  12  18  24  25]
