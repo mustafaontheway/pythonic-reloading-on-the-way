f11612da7020f77d344cd92f92992e4b6b0cd551
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-12-24 22:39:16 +0300

    Create part-x-0519-numpy-math1.py

---
+++ part-x-0519-numpy-math1.py
@@ -0,0 +1,22 @@
+import numpy as np 
+
+arr = np.random.randint(-12, 26, size=(6, 6))
+
+print(arr)
+
+print("------------------------------------------")
+
+print(np.add(arr, 2))
+
+print("------------------------------------------")
+
+print(np.subtract(arr, -3))
+
+print("------------------------------------------")
+
+print(np.multiply(arr, 4))
+
+print("------------------------------------------")
+
+print(np.divide(arr, 5))
+
