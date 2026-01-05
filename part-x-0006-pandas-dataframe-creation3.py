59bf220bf1aea51d0b1d786ecda9081777ffd4b4
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-05 19:36:09 +0300

    Create part-x-0006-pandas-dataframe-creation3.py

---
+++ part-x-0006-pandas-dataframe-creation3.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+import numpy as np
+
+years = np.array([2010, 2008, 2004])
+
+yearly_sales = np.array([45_000, 72_000, 54_300])
+
+df = pd.DataFrame({"years": years, "yearly_sales_amounts": yearly_sales})
+
+df.columns = ("important_years", "usd_yearly_sales")
+
+print(df)
+
+#    important_years  usd_yearly_sales
+# 0             2010             45000
+# 1             2008             72000
+# 2             2004             54300
