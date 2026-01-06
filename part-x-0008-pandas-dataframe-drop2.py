dfceec0de3b4c0db393cb8f01269b4e5047ae7d6
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-06 21:52:44 +0300

    Create part-x-0008-pandas-dataframe-drop2.py

---
+++ part-x-0008-pandas-dataframe-drop2.py
@@ -0,0 +1,27 @@
+import pandas as pd 
+import numpy as np
+
+years = np.array([2000, 2001, 2002, 2003, 2004, 2005, 2006])
+
+yearly_sales = np.array([45_000, 72_000, 54_300, 52_000, 47_400, 65_000, 60_000])
+
+num_of_emps = np.array([45, 48, 50, 44, 49, 54, 50])
+
+df = pd.DataFrame({"years": years, "yearly_sales_amounts": yearly_sales, "num_of_emps": num_of_emps})
+
+df.index = ["year1", "year2", "year3", "year4", "year5", "year6", "year7"]
+
+df.drop("year2", axis=0, inplace=True)
+
+print(df)
+
+"""
+       years  yearly_sales_amounts  num_of_emps
+year1   2000                 45000           45
+year3   2002                 54300           50
+year4   2003                 52000           44
+year5   2004                 47400           49
+year6   2005                 65000           54
+year7   2006                 60000           50
+
+"""
