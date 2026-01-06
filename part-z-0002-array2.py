42f3500f3fe61bc271c32cfa21bc5e61e514a0de
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-06 23:15:55 +0300

    Delete part-z-0002-array2.py

--- part-z-0002-array2.py
+++
@@ -1,9 +0,0 @@
-import array as arr
-
-are_emps_married = arr.array("b", [True, False, False, True, False])
-
-print(are_emps_married) # array('b', [1, 0, 0, 1, 0])
-
-profit_rates = arr.array("d", [0.12, 0.14, 0.08]) # float
-
-print(profit_rates) # array('d', [0.12, 0.14, 0.08])
