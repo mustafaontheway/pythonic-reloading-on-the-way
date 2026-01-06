6a1e5ce78254b600e518d336f91fd52381e0f865
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-06 23:15:35 +0300

    Delete part-z-0002-array1.py

--- part-z-0002-array1.py
+++
@@ -1,9 +0,0 @@
-import array as arr
-
-year_array = arr.array("i", [2000, 1990, 2026])
-
-#year_array.append("2025") # TypeError: 'str' object cannot be interpreted as an integer
-
-year_array.append(2025)
-
-print(year_array[3])
