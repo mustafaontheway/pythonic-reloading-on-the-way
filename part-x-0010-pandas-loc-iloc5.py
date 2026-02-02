0f0a116d5099ecc91339732e6993f1bc086ab484
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-02-02 17:44:03 +0300

    Create part-x-0010-pandas-loc-iloc5.py

---
+++ part-x-0010-pandas-loc-iloc5.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+fake_date = pd.DataFrame(np.random.randint(0, 100, 20).reshape(5, 4), columns=["c1", "c2", "c3", "c4"])
+
+fake_date.index = ["row1", "row2", "row3", "row4", "row5"]
+
+data1 = fake_date.loc["row1": "row3", "c1": "c2"]
+
+print(data1)
+
+#       c1  c2
+# row1  18  79
+# row2  98  76
+# row3  26   7
