cbf4e7290b440a6e906aaa5d3acfe9ad4a25f2cf
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-02-25 09:38:58 +0300

    Create part-d-0003-set-data-type1.py

---
+++ part-d-0003-set-data-type1.py
@@ -0,0 +1,15 @@
+quarterly_profit_rates = pl.Series(name = "profit rates", values = [12.34, 11.33, 14.24, 17.87], dtype = pl.Float32)
+
+print(quarterly_profit_rates)
+
+"""
+shape: (4,)
+Series: 'profit rates' [f32]
+[
+	12.34
+	11.33
+	14.24
+	17.870001
+]
+
+"""
