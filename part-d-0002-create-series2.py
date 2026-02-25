f861ecf50428db39bc193d2bd3087b91890a4306
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-02-25 09:29:03 +0300

    Create part-d-0002-create-series2.py

---
+++ part-d-0002-create-series2.py
@@ -0,0 +1,14 @@
+quarterly_profit_rates = pl.Series(name = "profit rates", values = [12.34, 11.33, 14.24, 17.87])
+
+print(quarterly_profit_rates)
+
+"""
+shape: (4,)
+Series: 'profit rates' [f64]
+[
+	12.34
+	11.33
+	14.24
+	17.87
+]
+"""
