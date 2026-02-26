9c8e77e8e8a1407e22328d58bef36d440b56587d
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-02-26 20:54:14 +0300

    Create  part-d-0008-read_csv1.py

---
+++ part-d-0008-read_csv1.py
@@ -0,0 +1,23 @@
+sales = pl.read_csv("salesdata.txt")
+
+print(sales)
+
+"""
+shape: (9, 2)
+┌──────────┬────────┐
+│ name     ┆ amount │
+│ ---      ┆ ---    │
+│ str      ┆ str    │
+╞══════════╪════════╡
+│ ayhan    ┆ 4000   │
+│ hakan    ┆ 3000   │
+│ bengu    ┆ 4100   │
+│ bilge    ┆  4400  │
+│ kutluk   ┆ 4800   │
+│ bumin    ┆ 2000   │
+│ teoman   ┆ 4980   │
+│ atilla   ┆ 4750   │
+│ kultigin ┆ 4648   │
+└──────────┴────────┘
+
+"""
